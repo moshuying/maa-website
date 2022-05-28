7a1b37b1db45b2ac85d0cc42d7d643af20417347
Liam Sho <conan_creeper@outlook.com>
2022-05-28 18:23:04 +0800

    feat.制作 Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   content: ['./{pages,components,layouts,plugins}/**/*.{vue,js,ts}', 'app.vue'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark',
+  },
 };
